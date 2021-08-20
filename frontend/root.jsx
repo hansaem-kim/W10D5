ad9a539c77e95884cea3b414921305c35d45c407
Hansaem Kim <hansaem_kim@hotmail.com>
2021-08-20 17:08:28 -0400

    working on tabs

--- frontend/root.jsx
+++ frontend/root.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import Clock from "./clock";
 
 const Root = (props) =>{
+    const tabList = [
+        {title: "kevin", content: "I am kevin"},
+        {title: "hansaem", content: "I am hansaem"}
+    ];
+
     return(
         <div>
             hi
             <Clock/>
+            <Tab tabList = {tabList}/>
         </div>
     );
 }
---
+++ frontend/tabs.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+// class Headers extends React.Component{
+//     constructor(props){
+//         // super(props);
+//         // this.state = {activeTab: 0};
+//     }
+
+//     render() {
+//         const activeTab = 
+//     }
+// }
+
+const Headers = (props) => {
+    <li></li>
+}
+
+class Tabs extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {tab: 0};
+    }
+
+
+
+    render() {
+        <div>
+            <ul>
+                <Headers
+                    title = {this.props.title}
+                    content = {this.props.content}
+                />
+            </ul>
+            <article></article>
+        </div>
+    }
+}
